2e04af0373616fc354df9e3bdd8dda7f54d961f4
staffm3 <staffm37@gmail.com>
2025-05-13 20:25:23 +0300

    fix task & task done

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,11 +1,17 @@
 import { isEmail } from "validator"
 import iziToast from "izitoast"
 import "izitoast/dist/css/iziToast.min.css"
+const storageKey = "feedback-form-state"
 const formData = {
-	email: "",
-	message: "",
+	email: JSON.parse(localStorage.getItem(storageKey))?.email ?? "",
+	message: JSON.parse(localStorage.getItem(storageKey))?.message ?? "",
 }
+localStorage.setItem(storageKey, JSON.stringify(formData))
 const form = document.querySelector(".feedback-form")
+;[...form.elements].map(item => {
+	if (item.nodeName !== "INPUT" && item.nodeName !== "TEXTAREA") return
+	item.value = formData[item.name]
+})
 const checkValidity = () => {
 	return (
 		formData.email.trim() === "" ||
@@ -13,7 +19,10 @@ const checkValidity = () => {
 		formData.message.trim() === ""
 	)
 }
-form.addEventListener("input", e => (formData[e.target.name] = e.target.value))
+form.addEventListener("input", e => {
+	formData[e.target.name] = e.target.value
+	localStorage.setItem(storageKey, JSON.stringify(formData))
+})
 form.addEventListener("submit", e => {
 	e.preventDefault()
 	if (checkValidity()) {
@@ -27,4 +36,10 @@ form.addEventListener("submit", e => {
 		title: "Yoopeee",
 		message: "Thank you for sending your message! 😊",
 	})
+	;[...form.elements].map(item => {
+		if (item.nodeName !== "INPUT" && item.nodeName !== "TEXTAREA") return
+		formData[item.name] = ""
+		item.value = ""
+	})
+	localStorage.removeItem(storageKey)
 })
